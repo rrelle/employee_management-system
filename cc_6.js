bea9829382af704980e01d2ab8853ae806267d26
Rrelle <rrelleferguson@usf.edu>
2026-03-01 20:18:46 -0500

    staged changes

---
+++ cc_6.js
@@ -0,0 +1,11 @@
+class Employee {
+    constructor(name, departtment) {
+        this.name = name;
+        this.department = department;
+    }
+
+    describe() {
+        return `${this.name} works in the ${this.department} department.`;
+    }
+}
+ 

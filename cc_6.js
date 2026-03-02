4dea2bf2471978d94fec0421a5181d7dd62e9ea2
Rrelle <rrelleferguson@usf.edu>
2026-03-01 20:25:14 -0500

    created class company and printed it to check

--- cc_6.js
+++ cc_6.js
@@ -15,4 +15,18 @@ class manager extends Employee {
         super(name, department);
         this.teamsize = teamsize;
     }   
+    describe() {
+        return `${this.name} works in the ${this.department} department and manages a team of ${this.teamsize} people.`;
+
+    }
+}
+class company {
+    constructor(name) {
+        this.name = name;
+        this.employees = [];
+    }
+    addEmployee(employee) {
+        this.employees.push(employee);
+        console.log(`${employee.name} has been added to the company.`);
+    }
     
